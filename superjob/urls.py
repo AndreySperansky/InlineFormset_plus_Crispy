86c9b0229a32089314b3d5569c2570988aab3dc7
Andrey.Speransky <andrey.speransky@gmail.com>
2021-03-27 15:37:17 +0300

    Создан проект и приложения

---
+++ superjob/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('mainapp.urls', namespace='main')),
+    path('auth/', include('authapp.urls', namespace='auth')),
+    path('employee/', include('employee.urls', namespace='employee')),
+    path('employer/', include('employer.urls', namespace='employer')),
+]
+
+if settings.DEBUG:      # т.е. если DEBUG = True
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
